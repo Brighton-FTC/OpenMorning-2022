bd194e1c36aba16fa86f20074832c534bfff4048
micnekr <misha.nekrasov@gmail.com>
2022-06-09 17:21:59 +0100

    Added more motor test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/testing/TestMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/testing/TestMotors.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teamcode.opModes.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.hardware.subsystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.hardware.subsystems.DriveTrainController;
 import org.firstinspires.ftc.teamcode.inputs.GamepadButton;
 import org.firstinspires.ftc.teamcode.inputs.Inputs;
+import org.firstinspires.ftc.teamcode.inputs.XY;
 import org.firstinspires.ftc.teamcode.inputs.inputs.DebouncedButton;
+import org.firstinspires.ftc.teamcode.inputs.inputs.ToggleableButton;
 import org.firstinspires.ftc.teamcode.wrappers.OpModeWrapper;
 
 @TeleOp(name = "Test motors", group = "Test")
@@ -19,16 +25,42 @@ public class TestMotors extends OpModeWrapper {
             "motor_2",
             "motor_3",
     };
+    String[] servoNames = {
+            "servo_0",
+            "servo_1",
+            "servo_2",
+            "servo_3",
+    };
+    CRServo[] crServos;
     DcMotor[] motors;
-    DebouncedButton[] motorButtons;
+    DebouncedButton[] selectionButtons;
+    ToggleableButton drivetrainToggleButton;
+
+    DriveTrainController driveTrain;
 
     @Override
     public void setup() {
         motors = new DcMotor[4];
+        crServos = new CRServo[4];
+
+        drivetrainToggleButton = new ToggleableButton(GamepadButton.L_BUMPER, false);
+
+        driveTrain = new DriveTrainController(new DriveTrain(
+                hardwareMap.get(DcMotor.class, motorNames[0]),
+                hardwareMap.get(DcMotor.class, motorNames[1]),
+                false
+        ),
+                Constants.TEAM1_DRIVETRAIN_COUNTS_PER_RADIAN,
+                Constants.TEAM1_DRIVETRAIN_COUNTS_PER_METER
+        );
+
+        for (int i = 0; i < motorNames.length; i++) {
+            crServos[i] = hardwareMap.get(CRServo.class, servoNames[i]);
+        }
         for (int i = 0; i < motorNames.length; i++) {
             motors[i] = hardwareMap.get(DcMotor.class, motorNames[i]);
         }
-        motorButtons = new DebouncedButton[] {
+        selectionButtons = new DebouncedButton[] {
                 new DebouncedButton(GamepadButton.TRIANGLE),
                 new DebouncedButton(GamepadButton.CIRCLE),
                 new DebouncedButton(GamepadButton.CROSS),
@@ -38,17 +70,33 @@ public class TestMotors extends OpModeWrapper {
 
     @Override
     public void loop() {
-        telemetry.addData("Selected motor: ", motorNames[motorId]);
+        boolean drivetrainButton = drivetrainToggleButton.processTick();
+        boolean isSelectingServos = Inputs.isPressed(GamepadButton.R_BUMPER);
+        telemetry.addData("Selected "+ (isSelectingServos ? "servo": "motor") + ": ", (isSelectingServos ? servoNames : motorNames)[motorId]);
+        telemetry.addData("Drivetrain enabled?", drivetrainButton);
         telemetry.addLine("Press triangle/circle/etc buttons");
 
         // select one of the motors
-        for (int i = 0; i < motorButtons.length; i++) if (motorButtons[i].processTick()) motorId = i;
+        for (int i = 0; i < selectionButtons.length; i++) if (selectionButtons[i].processTick()) motorId = i;
 
-        DcMotor motor = motors[motorId];
         double power = Inputs.getRightJoystickData().y;
-        telemetry.addData("Power", power);
-        telemetry.addData("Counts number", motor.getCurrentPosition());
-        motor.setPower(power);
+        if (isSelectingServos) {
+            CRServo servo = crServos[motorId];
+
+            servo.setPower(power);
+
+            telemetry.addData("Power", power);
+        } else {
+            DcMotor motor = motors[motorId];
+            motor.setPower(power);
+            telemetry.addData("Power", power);
+            telemetry.addData("Counts number", motor.getCurrentPosition());
+        }
+
+        if (drivetrainButton) {
+            XY leftJoystick = Inputs.getLeftJoystickData();
+            driveTrain.drive(-leftJoystick.y, leftJoystick.x);
+        }
 
         telemetry.update();
     }
